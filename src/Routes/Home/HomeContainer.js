603081ba26955887f058544f54bd8f0ea8665703
Dan Song <blueintelli@gmail.com>
2020-10-31 19:12:07 +0900

    #5.2 Home Container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,17 +1,44 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { moviesApi } from "../../api";
 
 export default class extends React.Component {
   state = {
-    nowPlaying: nulll,
-    upcoming: nulll,
-    popular: nulll,
+    nowPlaying: null,
+    upcoming: null,
+    popular: null,
     error: null,
     loading: true,
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await moviesApi.nowPlaying(); // 우리가 await 할 때, 자바스크립트는 일이 끝나기를 기다린다.
+      const {
+        data: { results: upcoming },
+      } = await moviesApi.upcoming();
+      const {
+        data: { results: popular },
+      } = await moviesApi.popular();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find movies information.",
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
+    console.log(this.state);
     return (
       <HomePresenter
         nowwPlaying={nowPlaying}
